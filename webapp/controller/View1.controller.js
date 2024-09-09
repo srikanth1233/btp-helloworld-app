cd4983a1c42c6f3688188a9c444c9c2ba119a5d5
Srikanth G <sgandla33@gmail.com>
2024-09-09 18:32:50 +0000

    intial commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("sap.btp.helloworldui5.controller.View1", {
+        onInit: function () {
+
+        }
+    });
+});
